82ef9c6f26a42f634a75d9fecb2aa134b1700b2b
Aleš Tanglmajer <ales.tanglmajer@gmail.com>
2021-11-24 12:55:23 +0100

    make main script into main function

--- main.py
+++ main.py
@@ -7,40 +7,46 @@ from physics import gravity, push_ball
 WIDTH = 800
 HEIGHT = 800
 
-pygame.init()
 
-window = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
+def main():
+    pygame.init()
 
-wall_list = [Wall((20, 20), (WIDTH - 20, 20)), Wall((WIDTH - 20, 20), (WIDTH - 20, HEIGHT - 20)),
-             Wall((WIDTH - 20, HEIGHT - 20), (20, HEIGHT - 20)), Wall((20, HEIGHT - 20), (20, 20)),
-             Wall((WIDTH - 50, 300), (WIDTH - 20, 300)), Wall((WIDTH - 50, 200), (WIDTH - 50, 350)),
-             Wall((WIDTH - 70, 290), (WIDTH - 50, 290))]
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+    clock = pygame.time.Clock()
 
-ball = Ball()
-hoop = Hoop((WIDTH - 120, 300), (WIDTH - 70, 300), wall_list)
+    wall_list = [Wall((20, 20), (WIDTH - 20, 20)), Wall((WIDTH - 20, 20), (WIDTH - 20, HEIGHT - 20)),
+                 Wall((WIDTH - 20, HEIGHT - 20), (20, HEIGHT - 20)), Wall((20, HEIGHT - 20), (20, 20)),
+                 Wall((WIDTH - 50, 300), (WIDTH - 20, 300)), Wall((WIDTH - 50, 200), (WIDTH - 50, 350)),
+                 Wall((WIDTH - 70, 290), (WIDTH - 50, 290))]
 
-while True:
-    clock.tick(60)
+    ball = Ball()
+    hoop = Hoop((WIDTH - 120, 300), (WIDTH - 70, 300), wall_list)
 
-    gravity(ball)
-    ball.update_position(wall_list)
+    while True:
+        clock.tick(60)
 
-    draw_screen(window, wall_list, hoop, ball)
+        gravity(ball)
+        ball.update_position(wall_list)
 
-    if ball.detect_hoop(hoop) is not None:
-        while True:
-            blit_text_center(window, "Score!")
+        draw_screen(window, wall_list, hoop, ball)
 
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    quit_game()
+        if ball.detect_hoop(hoop) is not None:
+            while True:
+                blit_text_center(window, "Score!")
 
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            quit_game()
+                for event in pygame.event.get():
+                    if event.type == pygame.QUIT:
+                        quit_game()
 
-        if event.type == pygame.KEYDOWN:
-            keys = pygame.key.get_pressed()
-            if keys[pygame.K_SPACE]:
-                push_ball(ball)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_game()
+
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_SPACE]:
+                    push_ball(ball)
+
+
+if __name__ == "__main__":
+    main()
