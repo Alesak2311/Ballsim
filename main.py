c19c67f9a279a554998163a63493180d881901e5
Aleš Tanglmajer <ales.tanglmajer@gmail.com>
2021-11-24 13:29:46 +0100

    choose angle function, each function now handles loop and framerate

--- main.py
+++ main.py
@@ -7,11 +7,40 @@ from physics import gravity, push_ball
 WIDTH = 800
 HEIGHT = 800
 
+clock = pygame.time.Clock()
 
-def simulation(window, wall_list, hoop, ball):
-    gravity(ball)
-    ball.update_position(wall_list)
-    draw_screen(window, wall_list, hoop, ball)
+
+def choose_angle(window, wall_list, hoop, ball):
+    angle = 0
+    ascending = True
+
+    done = False
+    while not done:
+        clock.tick(30)
+
+        draw_screen(window, wall_list, hoop, ball)
+        blit_text_center(window, f"Choose angle: {angle}")
+
+        if ascending:
+            angle += 1
+            if angle >= 90:
+                ascending = False
+        else:
+            angle -= 1
+            if angle <= 0:
+                ascending = True
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                quit_game()
+
+            if event.type == pygame.KEYDOWN:
+                keys = pygame.key.get_pressed()
+                if keys[pygame.K_SPACE]:
+                    done = True
+                    break
+
+    return angle
 
 
 def check_hoop(window, hoop, ball):
@@ -24,26 +53,16 @@ def check_hoop(window, hoop, ball):
                     quit_game()
 
 
-def main():
-    pygame.init()
-
-    window = pygame.display.set_mode((WIDTH, HEIGHT))
-    clock = pygame.time.Clock()
-
-    wall_list = [Wall((20, 20), (WIDTH - 20, 20)), Wall((WIDTH - 20, 20), (WIDTH - 20, HEIGHT - 20)),
-                 Wall((WIDTH - 20, HEIGHT - 20), (20, HEIGHT - 20)), Wall((20, HEIGHT - 20), (20, 20)),
-                 Wall((WIDTH - 50, 300), (WIDTH - 20, 300)), Wall((WIDTH - 50, 200), (WIDTH - 50, 350)),
-                 Wall((WIDTH - 70, 290), (WIDTH - 50, 290))]
-
-    ball = Ball()
-    hoop = Hoop((WIDTH - 120, 300), (WIDTH - 70, 300), wall_list)
-
+def simulation(window, wall_list, hoop, ball):
     while True:
         clock.tick(60)
 
-        simulation(window, wall_list, hoop, ball)
+        gravity(ball)
+        ball.update_position(wall_list)
 
         check_hoop(window, hoop, ball)
+
+        draw_screen(window, wall_list, hoop, ball)
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -53,6 +72,25 @@ def main():
                 keys = pygame.key.get_pressed()
                 if keys[pygame.K_SPACE]:
                     push_ball(ball)
+
+
+def main():
+    pygame.init()
+
+    window = pygame.display.set_mode((WIDTH, HEIGHT))
+
+    wall_list = [Wall((20, 20), (WIDTH - 20, 20)), Wall((WIDTH - 20, 20), (WIDTH - 20, HEIGHT - 20)),
+                 Wall((WIDTH - 20, HEIGHT - 20), (20, HEIGHT - 20)), Wall((20, HEIGHT - 20), (20, 20)),
+                 Wall((WIDTH - 50, 300), (WIDTH - 20, 300)), Wall((WIDTH - 50, 200), (WIDTH - 50, 350)),
+                 Wall((WIDTH - 70, 290), (WIDTH - 50, 290))]
+
+    ball = Ball()
+    hoop = Hoop((WIDTH - 120, 300), (WIDTH - 70, 300), wall_list)
+
+    choose_angle(window, wall_list, hoop, ball)
+
+    while True:
+        simulation(window, wall_list, hoop, ball)
 
 
 if __name__ == "__main__":
